e78c5e39a1e313f25ab6d5b1dba493044543eac7
Captain ALM <captainalm@captainalm.com>
2025-10-03 22:48:52 +0100

    Add key generation command generics with tests.

---
+++ crypto/cmd/key_gen_sig.go
@@ -0,0 +1,120 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/1f349/pqc-handshake/crypto"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func MainKegGenSig(scheme crypto.SigScheme, buildName, buildDate, buildVersion, buildAuthor, buildLicense string) {
+	mainKegGenSig(scheme, buildName, buildDate, buildVersion, buildAuthor, buildLicense, os.Exit, os.Stdout, os.Stdin)
+}
+
+func mainKegGenSig(scheme crypto.SigScheme, buildName, buildDate, buildVersion, buildAuthor, buildLicense string, exit func(code int), stdout *os.File, stdin *os.File) {
+	if len(os.Args) < 3 {
+		usageKG(buildName, buildDate, buildVersion, buildAuthor, buildLicense, exit)
+	} else {
+		if strings.EqualFold(os.Args[1], "g") || strings.EqualFold(os.Args[1], "gen") || strings.EqualFold(os.Args[1], "generate") {
+			var f *os.File
+			if os.Args[2] == "-" {
+				f = stdout
+			} else {
+				var err error
+				f, err = os.OpenFile(os.Args[2], os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0600)
+				if err != nil {
+					log.Println(err)
+					exit(2)
+					return
+				}
+			}
+			defer func() { _ = f.Close() }()
+			_, k, err := scheme.GenerateKey()
+			if err != nil {
+				log.Println(err)
+				exit(3)
+				return
+			}
+			var dat []byte
+			dat, err = k.MarshalBinary()
+			if err != nil {
+				log.Println(err)
+				exit(3)
+				return
+			}
+			_, err = f.Write(dat)
+			if err != nil {
+				log.Println(err)
+				exit(4)
+			}
+			_ = f.Sync()
+			exit(0)
+			return
+		} else if len(os.Args) > 3 && (strings.EqualFold(os.Args[1], "p") || strings.EqualFold(os.Args[1], "pub") || strings.EqualFold(os.Args[1], "public")) {
+			var f *os.File
+			if os.Args[2] == "-" {
+				f = stdin
+			} else {
+				var err error
+				f, err = os.Open(os.Args[2])
+				if err != nil {
+					log.Println(err)
+					exit(2)
+					return
+				}
+			}
+			defer func() { _ = f.Close() }()
+			var fo *os.File
+			if os.Args[3] == "-" {
+				fo = stdout
+			} else {
+				var err error
+				fo, err = os.OpenFile(os.Args[3], os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0600)
+				if err != nil {
+					log.Println(err)
+					exit(2)
+					return
+				}
+			}
+			defer func() { _ = fo.Close() }()
+			bts, err := io.ReadAll(f)
+			if err != nil {
+				log.Println(err)
+				exit(3)
+				return
+			}
+			k, err := scheme.UnmarshalBinaryPrivateKey(bts)
+			if err != nil {
+				log.Println(err)
+				exit(4)
+				return
+			}
+			var dat []byte
+			dat, err = k.Public().MarshalBinary()
+			if err != nil {
+				log.Println(err)
+				exit(5)
+				return
+			}
+			_, err = fo.Write(dat)
+			if err != nil {
+				log.Println(err)
+				exit(6)
+				return
+			}
+			_ = fo.Sync()
+			exit(0)
+			return
+		} else {
+			usageKG(buildName, buildDate, buildVersion, buildAuthor, buildLicense, exit)
+		}
+	}
+}
+
+func usageKG(buildName, buildDate, buildVersion, buildAuthor, buildLicense string, exit func(code int)) {
+	log.Printf("%s #%s (%s) : (C) %s : %s License\n", buildName, buildVersion, buildDate, buildAuthor, buildLicense)
+	fmt.Println("\nUsage:\n" + buildName + " <g/gen/generate> <private key path|->\n" + buildName + " <p/pub/public> <private key path|-> <public key path|->")
+	exit(1)
+}
---
+++ crypto/cmd/key_gen_test.go
@@ -0,0 +1,150 @@
+package cmd
+
+import (
+	"github.com/1f349/pqc-handshake/crypto"
+	"github.com/cloudflare/circl/kem/mlkem/mlkem768"
+	"github.com/cloudflare/circl/sign/mldsa/mldsa44"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"slices"
+	"testing"
+)
+
+func TestMainKegGenKem(t *testing.T) {
+	genericTest(t, &keyGenTestKem{})
+}
+
+func TestMainKegGenSig(t *testing.T) {
+	genericTest(t, &keyGenTestSig{})
+}
+
+type keyGenTest interface {
+	Main(buildName, buildDate, buildVersion, buildAuthor, buildLicense string, exit func(code int), stdout *os.File, stdin *os.File)
+}
+
+type keyGenTestKem struct{}
+
+func (k keyGenTestKem) Main(buildName, buildDate, buildVersion, buildAuthor, buildLicense string, exit func(code int), stdout *os.File, stdin *os.File) {
+	mainKegGenKem(crypto.WrapKem(mlkem768.Scheme()), buildName, buildDate, buildVersion, buildAuthor, buildLicense, exit, stdout, stdin)
+}
+
+type keyGenTestSig struct{}
+
+func (k keyGenTestSig) Main(buildName, buildDate, buildVersion, buildAuthor, buildLicense string, exit func(code int), stdout *os.File, stdin *os.File) {
+	mainKegGenSig(crypto.WrapSig(mldsa44.Scheme()), buildName, buildDate, buildVersion, buildAuthor, buildLicense, exit, stdout, stdin)
+}
+
+func genericTest(t *testing.T, test keyGenTest) {
+	dir := t.TempDir()
+	var genCmds = []string{"g", "gen", "generate", "G", "gEn", "geNERate"}
+	var pubCmds = []string{"p", "pub", "public", "P", "pUb", "puBLIc"}
+	var oargs = os.Args
+	defer func() {
+		os.Args = oargs
+	}()
+	var tFunc = func(code int) {
+		if code != 0 {
+			t.Log(code)
+			t.FailNow()
+		}
+	}
+	var uFunc = func(code int) {
+		if code != 1 {
+			t.Log(code)
+			t.FailNow()
+		}
+	}
+	for idx, genCmd := range genCmds {
+		t.Run("command - "+genCmd+" - stdout", func(t *testing.T) {
+			os.Args = []string{"testing", genCmd, "-"}
+			test.Main("a", "b", "c", "d", "e", tFunc, getStdOut(dir), nil)
+		})
+		t.Run("command - "+genCmd+" - file", func(t *testing.T) {
+			os.Args = []string{"testing", genCmd, dir + "/" + genCmd}
+			test.Main("a", "b", "c", "d", "e", tFunc, nil, nil)
+		})
+		assert.NoError(t, copyStdOutToIn(dir))
+		t.Run("command - "+pubCmds[idx]+" - stdin - stdout", func(t *testing.T) {
+			os.Args = []string{"testing", pubCmds[idx], "-", "-"}
+			test.Main("a", "b", "c", "d", "e", tFunc, getStdOut(dir), getStdIn(dir))
+		})
+		t.Run("command - "+pubCmds[idx]+" - stdin - file", func(t *testing.T) {
+			os.Args = []string{"testing", pubCmds[idx], "-", dir + "/" + pubCmds[idx]}
+			test.Main("a", "b", "c", "d", "e", tFunc, nil, getStdIn(dir))
+		})
+		bts, err := os.ReadFile(dir + "/" + pubCmds[idx])
+		assert.NoError(t, err)
+		assert.True(t, checkStdOut(dir, bts))
+		t.Run("command - "+pubCmds[idx]+" - file - stdout", func(t *testing.T) {
+			os.Args = []string{"testing", pubCmds[idx], dir + "/" + genCmd, "-"}
+			test.Main("a", "b", "c", "d", "e", tFunc, getStdOut(dir), nil)
+		})
+		t.Run("command - "+pubCmds[idx]+" - file - file", func(t *testing.T) {
+			os.Args = []string{"testing", pubCmds[idx], dir + "/" + genCmd, dir + "/" + pubCmds[idx]}
+			test.Main("a", "b", "c", "d", "e", tFunc, nil, nil)
+		})
+		bts, err = os.ReadFile(dir + "/" + pubCmds[idx])
+		assert.NoError(t, err)
+		assert.True(t, checkStdOut(dir, bts))
+	}
+	t.Run("usage", func(t *testing.T) {
+		os.Args = []string{"testing"}
+		test.Main("a", "b", "c", "d", "e", uFunc, nil, nil)
+	})
+	t.Run("invalid", func(t *testing.T) {
+		os.Args = []string{"testing", "abc"}
+		test.Main("a", "b", "c", "d", "e", uFunc, nil, nil)
+	})
+}
+
+func copyStdOutToIn(dir string) error {
+	f, err := os.Open(dir + "/stdout")
+	if err != nil {
+		return err
+	}
+	defer func() { _ = f.Close() }()
+	o, err := os.OpenFile(dir+"/stdin", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return nil
+	}
+	defer func() { _ = o.Close() }()
+	_, err = io.Copy(o, f)
+	return err
+}
+
+func getStdOut(dir string) *os.File {
+	f, err := os.OpenFile(dir+"/stdout", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return nil
+	}
+	return f
+}
+
+func checkStdOut(dir string, bts []byte) bool {
+	f, err := os.Open(dir + "/stdout")
+	if err != nil {
+		return false
+	}
+	defer func() { _ = f.Close() }()
+	chk, err := io.ReadAll(f)
+	return slices.Equal(bts, chk)
+}
+
+func getStdIn(dir string) *os.File {
+	f, err := os.Open(dir + "/stdin")
+	if err != nil {
+		return nil
+	}
+	return f
+}
+
+func writeStdIn(dir string, bts []byte) error {
+	f, err := os.OpenFile(dir+"/stdin", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = f.Close() }()
+	_, err = f.Write(bts)
+	return err
+}
